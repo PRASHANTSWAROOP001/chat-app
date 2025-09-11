7363d4d479a3629a0b7276bc12f2bb326a5d955c
PRASHANTSWAROOP001 <mrprashantswaroop@gmail.com>
2025-09-12 01:43:56 +0530

    now schema.prisma created binary only for alpine + windows only
    now redis is not entirely hardcoded its host and port is configured using the .env

--- src/utils/db/db.ts
+++ src/utils/db/db.ts
@@ -1,9 +1,19 @@
 import { PrismaClient } from "@prisma/client";
-import Redis from "ioredis"
+import Redis from "ioredis";
 
 export const prismaClient = new PrismaClient();
-// redis connection to be used as publisher and normal event
-export const redis = new Redis(6380)
 
-// redis connection to be used as subscriber and handle the subcribe methods only.
-export const subscriber = new Redis(6380)
+const redisHost = process.env.REDIS_HOST || "127.0.0.1";
+const redisPort = Number(process.env.REDIS_PORT) || 6379;
+
+// redis connection to be used as publisher and normal event
+export const redis = new Redis({
+  host: redisHost,
+  port: redisPort,
+});
+
+// redis connection to be used as subscriber and handle the subscribe methods only.
+export const subscriber = new Redis({
+  host: redisHost,
+  port: redisPort,
+});
